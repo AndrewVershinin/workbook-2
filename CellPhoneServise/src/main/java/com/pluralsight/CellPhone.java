9908154539e3be122dc97705f60e077a098c5681
Anrew <andrvolg.job@gmail.com>
2025-04-16 15:52:13 -0400

    set setters

--- CellPhoneServise/src/main/java/com/pluralsight/CellPhone.java
+++ CellPhoneServise/src/main/java/com/pluralsight/CellPhone.java
@@ -48,4 +48,20 @@ public class CellPhone {
     public void setSerialNumber(int serialNumber) {
         this.serialNumber = serialNumber;
     }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
