24346538fa7eeae373b42f0b9c0f64143faebb21
Anrew <andrvolg.job@gmail.com>
2025-04-17 11:43:27 -0400

    Do While, For, While was remembered

---
+++ LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int myLove = 0;
+        do {
+            System.out.println("Love Java");
+            myLove ++;
+        } while (myLove < 5);
+    }
+}
